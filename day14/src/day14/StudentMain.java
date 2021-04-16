4ec8ad4da288d71e3d00d3c0b1f0f0943616045e
user <user@DESKTOP-T4A95VB>
2021-04-16 12:53:13 +0900

    get , set 개념

--- day14/src/day14/StudentMain.java
+++ day14/src/day14/StudentMain.java
@@ -7,11 +7,19 @@ public class StudentMain {
 	public static void main(String[] args) {
 		
 		Student s = new Student();
+//		setter를 이용하여 필드값 지정		
+//		s.name = "학생";
+		s.setName("학생");
+//		s.phone = "010-1111-2222";
+		s.setPhone("010-1111-2222");
+//		s.address = "인천";
+		s.setAddress("인천");
 		
-		s.name = "학생";
-		s.phone = "010-1111-2222";
-		s.address = "인천";
+		// getter를 이용하여 필드값 가져오기
+		String var2 = s.getName();
+		System.out.println(var2);
 		
+		System.out.println(s.getAddress());
 						
 //		List<String> list = new ArrayList<String>();
 //		list.add(s.name);
@@ -32,14 +40,14 @@ public class StudentMain {
 		stuList.add(s2);
 		
 		// stuList의 0번 인덱스에 저장한 s 의 name값 출력해보기
-		System.out.println(stuList.get(0).name);
-		System.out.println(stuList.get(1).name);
+		System.out.println(stuList.get(0).getName());
+		System.out.println(stuList.get(1).getName());
 	
-		String var1 = stuList.get(0).name;
+		String var1 = stuList.get(0).getName();
 		System.out.println(var1);
 		
-		stuList.get(1).name = "학생2이름변경";
-		System.out.println(stuList.get(1).name);
+		stuList.get(1).setName("학생2이름변경"); 
+		System.out.println(stuList.get(1).getName());
 		
 		
 		
